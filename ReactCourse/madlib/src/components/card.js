58a358ddfd4d31cc8a6b94d407a0f9c36d155d9b
Code <Code@batc.local>
2018-07-02 11:59:20 -0600

    7/2/2018 - Completed Madlib React Project(Still a few tweaks I could mess with though, like 14

--- ReactCourse/madlib/src/components/card.js
+++ ReactCourse/madlib/src/components/card.js
@@ -3,31 +3,33 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '',
+    adjectiveOne: '',
+    celebOne: '',
+    adjectiveTwo: '',
+    nounOne: '',
+    numberOne: '',
+    numberTwo: '',
+    nounTwo: '',
+    adjectiveThree: '',
+    celebTwo: '',
+    celebThree: '',
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+    contentVisible: false,
+}
+
+
 class Card extends Component {
 
     constructor() {
         super()
 
-         this.state = {
-            color: '',
-            pluralNoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFive: '',
-            contentVisible: false
-            
-        };
+        this.state = INITIAL_STATE;
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -39,7 +41,11 @@ class Card extends Component {
 
     handleFormSubmit(event) {
         event.preventDefault();
-        this.setState({ contentVisible: !this.state.contentVisible}) 
+        if (this.state.contentVisible) {
+            this.setState(INITIAL_STATE)
+        } else {
+            this.setState({ contentVisible: true})
+        } 
     }
 
     render() {
@@ -58,21 +64,24 @@ class Card extends Component {
             {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
             {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
             {title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
-            {title: 'Celebrity', state: this.state.celebThree, name: 'CelebThree'},
+            {title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
 
             {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
             {title: 'Noun', state: this.state.nounThrees, name: 'nounThree'},
             {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
             {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'},
         ]
+        
         return (
             <form onSubmit = {this.handleFormSubmit} className = 'card'>
                 <div className = 'card__inputs'>
                 {
-                    inputData.map(data => Input((data), this.handleInputChange))
+                    inputData.map((data, index) => {
+                        return Input((data), this.handleInputChange, index);
+                    })
                 }
                 </div>
-                <button type = 'submit'>{!this.state.contenVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
+                <button className = {`card__${!this.state.contentVisible ? 'generate' : 'clear'}`}type = 'submit'>{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
                 {
                     this.state.contentVisible ? <Content data = {this.state}/> : <div></div>
                 }
--- ReactCourse/madlib/src/components/content.js
+++ ReactCourse/madlib/src/components/content.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 
 class Content extends Component {
+    constructor() {
+        super()
+        this.count = 1;
+    }
+
+    componentWillUpdate() {
+        this.count = 1
+    }
+
+    renderLabel(data) {
+        return [
+            <div className= 'card__content__label'>{this.count++}</div>,
+            <b>{data}</b>
+        ]
+    }
+
     render() {
-        console.log(this.props.data);
-        const data = this.props.data
+        const data = this.props.data;
+        
         return (
-            <p>
-               {/* Ladies and gentlemen, this is {this.renderLabel(data.color)} Barber, your sportscaster, bringing you the last inning of the game between the Cleveland {this.renderLabel(data.pluralNoun)} and the {this.renderLabel(data.adjectiveOne)} Yankees. {this.renderLabel(data.celebOne)} is pitching for the Yankees. Here's the pitch! It's a low {this.renderLabel(data.adjectiveTwo)} ball that just cuts the inside of the {this.renderLabel(data.nounOne)} for a strike. That makes the count {this.renderLabel(data.numberOne)} strikes and {this.renderLabel(data.numberTwo)} balls. Now here's the next pitch. The batter swings and connects. It's a long, high {this.renderLabel(data.nounTwo)} out to {this.renderLabel(data.adjectiveThree)} field. But {this.renderLabel(data.celebTwo)} is coming up fast and has it for the second out. The next batter up is {this.renderLabel(data.celebThree)}, the Cleveland {this.renderLabel(data.adjectiveFour)}-stop. Here's the pitch... and it's hit... a short ground ball to third {this.renderLabel(data.nounThree)}. {this.renderLabel(data.celebFour)} scoops it up and throws it to first base for an out, and the game is over. And the Yankees move into second place in the {this.renderLabel(data.adjectiveFive)} League!   */}
-               Hey {data.color} dumb head, what's cracka lacking you stinking fart knuckle
+            <p className = 'card__content'>
+               Ladies and gentlemen, this is {this.renderLabel(data.color)} Barber, your sportscaster, bringing you the last inning of the game between the Cleveland {this.renderLabel(data.pluralNoun)} and the {this.renderLabel(data.adjectiveOne)} Yankees. {this.renderLabel(data.celebOne)} is pitching for the Yankees. Here's the pitch! It's a low {this.renderLabel(data.adjectiveTwo)} ball that just cuts the inside of the {this.renderLabel(data.nounOne)} for a strike. That makes the count {this.renderLabel(data.numberOne)} strikes and {this.renderLabel(data.numberTwo)} balls. Now here's the next pitch. The batter swings and connects. It's a long, high {this.renderLabel(data.nounTwo)} out to {this.renderLabel(data.adjectiveThree)} field. But {this.renderLabel(data.celebTwo)} is coming up fast and has it for the second out. The next batter up is {this.renderLabel(data.celebThree)}, the Cleveland {this.renderLabel(data.adjectiveFour)}-stop. Here's the pitch... and it's hit... a short ground ball to third {this.renderLabel(data.nounThree)}. {this.renderLabel(data.celebFour)} scoops it up and throws it to first base for an out, and the game is over. And the Yankees move into second place in the {this.renderLabel(data.adjectiveFive)} League!  
+               
             </p>
         )
     }
