b09d84b04e1596150268b8500df6a951469b6790
Sean Collings <seanrcollings@gmail.com>
2018-07-04 18:11:31 -0600

    7/4/2018 - completed Birthday Countdown

--- ReactCourse/countdown/src/components/app.js
+++ ReactCourse/countdown/src/components/app.js
@@ -12,7 +12,7 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     
-    this.timer = 0
+    this.timer = 0;
 
     this.state = {
       active: false,
@@ -23,7 +23,9 @@ export default class App extends Component {
         minutes: 0,
         seconds: 0
       }
-    }
+    };
+
+    this.age = 0;
   }
 
   handleChange = function(date) {
@@ -34,11 +36,38 @@ export default class App extends Component {
   }.bind(this)
 
   handleGenerate = function() {
-    this.setState({ active: true })
-    var countDownDate = this.state.startDate.toDate().getTime();
+    
+    var bday = this.state.startDate.toDate();
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var birthdayMonth = bday.getMonth();
+
+    var timeBetween = today.getTime() - bday.getTime();
+    var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
+    var age = Number((daysOld/365).toFixed(0)) + 1; // Fix the + 1, sort of a hack because this is a mess of variables
+    
+    this.setState({ age, active: true})
+
+    if (birthdayMonth > currentMonth) {
+      bday.setFullYear(today.getFullYear())
+    } else if (birthdayMonth < currentMonth) {
+      bday.setFullYear(today.getFullYear() + 1)
+    } else if (birthdayMonth === currentMonth) {
+      var currentDay = today.getDate();
+      var birthDay = bday.getDate()
+
+      if (birthDay > currentDay) {
+        bday.setFullYear(today.getFullYear())
+      } else if (birthDay <= currentDay) {
+        bday.setFullYear(today.getFullYear() + 1)
+      }
+    }
+
+
+    var countDownDate = bday.getTime();
 
     this.timer = setInterval(function(){
-      var now = new Date().getTime();
+      var now = moment().toDate().getTime();
       var distance = countDownDate - now;
 
       var days    =  Math.floor(distance  / (1000 * 60 * 60 * 24));
@@ -46,8 +75,6 @@ export default class App extends Component {
       var minutes =  Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds =  Math.floor((distance % (1000 * 60)) / 1000);
       
-      const time = days + 'd ' + hours + 'h '+ minutes + 'm ' + seconds + 's';
-
       this.setState({
         timeRemaining: {
           days,
@@ -64,18 +91,38 @@ export default class App extends Component {
     }.bind(this), 1000)
   }.bind(this)
 
+  getBirthDate = function(date) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    if (month < 10) {
+      return `0${month}/${day}`  
+    } else {
+      return `${month}/${day}`
+    }
+
+
+    // const month = ( date.getMonth().toString().length === 1 ?
+    //  `0${date.getMonth() + 1}` : 
+    //  (date.getMonth() + 1).toString() );
+
+    // const day = ( date.getDay().toString().length === 1 ? 
+    // `0${date.getDay() + 1}` : 
+    // (date.getDay() + 1).toString() );
+
+  }.bind(this)
+
   renderItems = function() {
     if(this.state.active) {
       return [
-        <Clock timeRemaining = {this.state.timeRemaining}/>,
+        <Clock key = {0} timeRemaining = {this.state.timeRemaining}/>,
         ChangeDate('Change Date', () => this.setState({ active: false })),
-        LargeText('04/13'),
-        <label className = 'grid__remaining'>Remaining until you 18th birthday</label>
+        LargeText(this.getBirthDate(this.state.startDate.toDate())),
+        <label key = {3} className = 'grid__remaining'>Remaining until you turn {this.state.age}</label>
       ]
     } else {
       return [
         Button('Generate Countdown', () => this.handleGenerate()),
-        <Picker startDate = {this.state.startDate} callback = {(date) => this.handleChange(date)}/>
+        <Picker key = {1} startDate = {this.state.startDate} callback = {(date) => this.handleChange(date)}/>
       ]
     }
   }.bind(this)
@@ -85,7 +132,6 @@ export default class App extends Component {
     return (
       <div className = 'grid'>
         <h1 className = 'grid__title'>Birthday Countdown</h1>
-        
         <div className = 'grid__skew-dark-two'></div>
 
         <div className = 'grid__skew-light-one'></div>
